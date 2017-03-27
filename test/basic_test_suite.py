c32aedec60510fdf07531f0de7b8e220ee339155
Don Munro <dmunro@siaras.com>
2017-03-26 20:07:34 -0700

    Added basic unit tests

---
+++ test/basic_test_suite.py
@@ -0,0 +1,40 @@
+import unittest
+from lotto_picker.lotto_picker import lotto
+
+
+class LottoTests(unittest.TestCase):
+
+    def setUp(self):
+        self.lotto_picker = lotto.Lotto()
+
+    def test_with_chars(self):
+        # ensures non-numeric strings are ignored
+        picker = lotto.Lotto()
+        numbers_map = picker.check_number_strings(['bogusString', '1234567'])
+        self.assertTrue(numbers_map.get('1234567'))
+        self.assertFalse(numbers_map.get('bogusString'))
+
+    def test_multiple_lottos(self):
+        # ensures that all possible lotto numbers are produced from a string
+        # that is capable of producing > 1 set of numbers.
+        picker = lotto.Lotto()
+        numbers_map = picker.check_number_strings(['4938532814754'])
+        length = len(numbers_map.get('4938532814754'))
+        self.assertTrue(length == 2)
+
+    def test_repeated_numbers(self):
+        # ensures that a string does not generate a set of lotto numbers with
+        # duplicate numbers.
+        picker = lotto.Lotto()
+        numbers_map = picker.check_number_strings(['1234123'])
+        self.assertFalse(numbers_map.get('1234123'))
+
+    def test_too_long_too_short(self):
+        # ensures that strings that are too short or too long are ignored
+        picker = lotto.Lotto()
+        numbers_map = picker.check_number_strings(['123456', '123456789123456'])
+        self.assertFalse(numbers_map)
+
+
+if __name__ == '__main__':
+    unittest.main()
